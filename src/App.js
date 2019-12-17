5b583595a42eb25f87cce53f93c29b1e2e0fbbec
alex pereira <alexapereira14@gmail.com>
2019-12-16 21:25:41 -0500

    primitive list for tunings... doesnt do anything yet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import { TuningSelector } from './components/TuningSelector';
 import { PlayablePiano } from './components/PlayablePiano';
 
 const App = () => {
   return (
     <div>
+      <h1>Alternate Tuning Piano</h1>
+      <TuningSelector />
       <PlayablePiano />
     </div>
   );
